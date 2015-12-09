abb4041263f7e9455bd320003fb873be8773ba1f
Tom Elliott <telliott@yext.com>
2015-12-09 11:38:42 -0500

    Fix address based search test after changes to underlying data and add a city-based search test. r=jfancher

--- geocoding_api_test.go
+++ geocoding_api_test.go
@@ -1,6 +1,7 @@
 package mapbox
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -65,18 +66,18 @@ func TestQueryByAddress(t *testing.T) {
 	if res == nil {
 		t.Fatalf("expected result, got: %v", res)
 	}
-	if len(res.Features) != 1 {
-		t.Fatalf("expected 1 feature, got: %v", len(res.Features))
+	if len(res.Features) < 1 {
+		t.Fatalf("expected at least 1 feature, got: %v", len(res.Features))
 	}
 	f := res.Features[0]
 	if len(f.Center) != 2 {
 		t.Errorf("expected center with 2 coordinates, got: %v", len(f.Center))
 	} else {
-		if f.Center[0] != 11.54165 {
-			t.Errorf("expected center longitude of 11.54165, got: %v", f.Center[0])
+		if f.Center[0] != 11.541783 {
+			t.Errorf("expected center longitude of 11.541783, got: %v", f.Center[0])
 		}
-		if f.Center[1] != 48.151313 {
-			t.Errorf("expected center latitude of 48.151313 , got: %v", f.Center[1])
+		if f.Center[1] != 48.152471 {
+			t.Errorf("expected center latitude of 48.152471 , got: %v", f.Center[1])
 		}
 	}
 	if f.PlaceName != "Munich, Bayern, Germany" {
@@ -85,8 +86,55 @@ func TestQueryByAddress(t *testing.T) {
 	if f.Text != "Munich" {
 		t.Errorf("expected text of %q, got: %v", "Munich", f.Text)
 	}
-	if f.ID != "city.10676757" {
-		t.Errorf("expected id %q, got: %v", "city.10676757", f.PlaceName)
+	if f.ID != "city.676757" {
+		t.Errorf("expected id %q, got: %v", "city.676757", f.ID)
+	}
+	if len(f.BoundingBox) != 4 {
+		t.Errorf("expected bbox with 4 coordinates, got: %v", len(f.BoundingBox))
+	}
+}
+
+func TestQueryByCity(t *testing.T) {
+	accessToken, err := readAccessToken(t)
+	if err != nil {
+		t.Fail()
+		return
+	}
+
+	geocoder := NewClient(accessToken).Geocoding()
+	req := &QueryByAddressRequest{
+		Index: "mapbox.places-v1",
+		Query: "Munich",
+	}
+	res, err := geocoder.QueryByAddress(req)
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+	if res == nil {
+		t.Fatalf("expected result, got: %v", res)
+	}
+	if len(res.Features) < 1 {
+		t.Fatalf("expected at least 1 feature, got: %v", len(res.Features))
+	}
+	f := res.Features[0]
+	if len(f.Center) != 2 {
+		t.Errorf("expected center with 2 coordinates, got: %v", len(f.Center))
+	} else {
+		if f.Center[0] != 11.541783 {
+			t.Errorf("expected center longitude of 11.541783, got: %v", f.Center[0])
+		}
+		if f.Center[1] != 48.152471 {
+			t.Errorf("expected center latitude of 48.152471 , got: %v", f.Center[1])
+		}
+	}
+	if !strings.Contains(f.PlaceName, req.Query) {
+		t.Errorf("expected place name to contain %q, got: %v", req.Query, f.PlaceName)
+	}
+	if f.Text != "Munich" {
+		t.Errorf("expected text of %q, got: %v", "Munich", f.Text)
+	}
+	if f.ID != "city.676757" {
+		t.Errorf("expected id %q, got: %v", "city.676757", f.ID)
 	}
 	if len(f.BoundingBox) != 4 {
 		t.Errorf("expected bbox with 4 coordinates, got: %v", len(f.BoundingBox))
